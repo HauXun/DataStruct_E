520f5b9359157fb6f12a5cb11f9abf66fc93a58c
HauXun <hautrieu65@gmail.com>
2021-05-02 19:32:54 +0700

    Init Commit 🚀🚀🚀

---
+++ Lab06/Lab06_D_Bai03/Lab06_D_Bai03/Lab06_D_Bai03.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+
+#include "thuvien.h"
+
+int main()
+{
+	STACK s;
+	CreatStack(s);
+	Input(s);
+	Output(s);
+	return 1;
+}
